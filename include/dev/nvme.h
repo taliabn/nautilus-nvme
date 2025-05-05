ed17604d2406031db752c5220db9ce0c35ea2d72
Talia Ben-Naim <taliabennaim@gmail.com>
2025-05-04 21:42:04 -0500

    defined types for nvme commands

--- include/dev/nvme.h
+++ include/dev/nvme.h
@@ -29,4 +29,58 @@
 int  nk_nvme_init(struct naut_info *naut);
 void nk_nvme_deinit();
 
+/* NVMe types*/
+
+/* Submission queue entries */
+struct nvme_command {
+    uint8_t opc;		/* opcode */
+    uint8_t flags;		/* fused operation */
+    uint16_t cid;		/* command identifier */
+    uint32_t nsid;		/* namespace identifier */
+    uint32_t rsvd2;		/* reserved */
+    uint32_t rsvd3;		/* reserved */
+    uint64_t metadata;	/* metadata pointer */
+    /* data pointer:
+       some NVMe implementations also support scatter-gather lists 
+       but for now we're just using physical page regions (prp)*/
+    uint64_t prp1;		/* prp entry 1 */
+    uint64_t prp2;		/* prp entry 2 */
+    uint32_t cdw10;		/* command-specific */
+    uint32_t cdw11;		/* command-specific */
+    uint32_t cdw12;		/* command-specific */
+    uint32_t cdw13;		/* command-specific */
+    uint32_t cdw14;		/* command-specific */
+    uint32_t cdw15;		/* command-specific */
+};
+
+/* command opcodes*/
+enum nvme_opcode {
+    NVME_OPC_FLUSH  		= 0x00,
+    NVME_OPC_WRITE  		= 0x01,
+    NVME_OPC_READ   		= 0x02,
+    NVME_OPC_WRITE_UNCOR    = 0x04,
+    NVME_OPC_COMPARE    	= 0x05,
+    NVME_OPC_WRITE_ZEROES  	= 0x08,
+    NVME_OPC_DSM    		= 0x09,
+    NVME_OPC_VERIFY 		= 0x0c,
+    NVME_OPC_RESV_REGISTER  = 0x0d,
+    NVME_OPC_RESV_REPORT    = 0x0e,
+    NVME_OPC_RESV_ACQUIRE   = 0x11,
+    NVME_OPC_RESV_RELEASE   = 0x15,
+    NVME_OPC_ZONE_MGMT_SEND = 0x79,
+    NVME_OPC_ZONE_MGMT_RECV = 0x7a,
+    NVME_OPC_ZONE_APPEND    = 0x7d,
+    NVME_OPC_VENDOR_START   = 0x80,
+};
+
+/* completion queue entry */
+struct nvme_completion {
+    uint32_t cdw0;		/* command-specific */
+    uint32_t rsvd1;		/* reserved */
+    uint16_t sq_head;	/* submission queue head pointer */
+    uint16_t sq_id;		/* submission queue identifier */
+    uint16_t cid;		/* command identifier */
+    uint16_t status; 	/* did the command fail, and if so, why? */
+};
+
 #endif
